3c91c0809e43fdfd8234da230450c37fdf598761
Andrés Castillo <holalenniton@gmail.com>
2024-10-27 00:09:23 -0500

    feat(backend): implemento jwt junto a rutas,controlador y métodos de autenticación

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request){
+
+        $credencials = $request->only('email','password');
+
+        if(!$token = JWTAuth::attempt($credencials)){
+            return response()->json([
+                'message' => 'Las credenciales son incorrectas'
+            ],Response::HTTP_UNPROCESSABLE_ENTITY); //422
+        }
+
+        return response()->json(compact('token'),Response::HTTP_OK);;
+
+    }
+
+    public function logout(){
+
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        return response()->json(['message' => 'El usuario se ha deslogueado satisfactoriamente'], Response::HTTP_OK);
+
+    }
+
+    public function register(Request $request){
+
+        $user = User::create($request->all());
+
+        return response()->json($user,Response::HTTP_CREATED);
+
+    }
+
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,18 +3,24 @@
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\TaskPriorityController;
 use App\Http\Controllers\Api\TaskStatusController;
-use App\Models\TaskPriority;
-use App\Models\TaskStatus;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Public
+Route::post('login',[AuthController::class,'login']);
+Route::post('logout',[AuthController::class,'logout']);
+Route::post('register',[AuthController::class,'register']);
+
+//Private
+Route::middleware('jwt.auth')->group(function(){
+    Route::apiResource('tasks',TaskController::class);
+
+    Route::get('task_priorities',[TaskPriorityController::class,'index']);
+    Route::get('task_priorities/{task_priority}',[TaskPriorityController::class,'show']);
+
+    Route::get('task_statuses',[TaskStatusController::class,'index']);
+    Route::get('task_statuses/{task_status}',[TaskStatusController::class,'show']);
+});
 
 
-
-Route::apiResource('tasks',TaskController::class);
-
-Route::get('task_priorities',[TaskPriorityController::class,'index']);
-Route::get('task_priorities/{task_priority}',[TaskPriorityController::class,'show']);
-
-Route::get('task_statuses',[TaskStatusController::class,'index']);
-Route::get('task_statuses/{task_status}',[TaskStatusController::class,'show']);
