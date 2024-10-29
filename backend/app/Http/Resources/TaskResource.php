e04c87f4e7d8ef05ca1f472511de3571929bb7ce
Andrés Castillo <holalenniton@gmail.com>
2024-10-29 13:01:43 -0500

    feat(backend): refactorizamos el recurso de tarea, dandole un mejor formato para la renderización de la tabla en el frontend

--- backend/app/Http/Resources/TaskResource.php
+++ backend/app/Http/Resources/TaskResource.php
@@ -16,17 +16,12 @@ class TaskResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'type' => 'task',
-            'attributes' => [
-                'title' => $this->title,
-                'description' => $this->description,
-                'end_date' => $this->end_date
-            ],
-            'relationships' => [
-                'user' => $this->user?->name,
-                'task_status' => $this->task_status?->name,
-                'task_priority' => $this->task_priority?->name
-            ]
+            'title' => $this->title,
+            'description' => $this->description,
+            'end_date' => $this->end_date,
+            'user' => $this->user?->name,
+            'task_status' => $this->task_status?->name,
+            'task_priority' => $this->task_priority?->name
         ];
     }
 }
